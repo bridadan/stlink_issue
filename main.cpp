8c7be245e9987f2450ecb260cba5a596f9cad89d
Brian Daniels <brianddaniels@gmail.com>
2018-10-10 16:10:06 -0500

    Removing RawSerial

--- main.cpp
+++ main.cpp
@@ -1,8 +1,6 @@
 #include "mbed.h"
 #include "greentea_serial.h"
 
-RawSerial pc(USBTX, USBRX);
-
 int main() {
     greentea_serial->printf("Listening, try entering characters to see them echoed.\r\n");
     while(1) {
