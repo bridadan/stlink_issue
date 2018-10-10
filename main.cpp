f7e01b03c7b4d410eac23efb4e07aa35c4bea635
Brian Daniels <brianddaniels@gmail.com>
2018-10-10 16:09:02 -0500

    Using greentea_serial

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 #include "mbed.h"
+#include "greentea_serial.h"
 
 RawSerial pc(USBTX, USBRX);
 
 int main() {
-    pc.printf("Listening, try entering characters to see them echoed.\r\n");
+    greentea_serial->printf("Listening, try entering characters to see them echoed.\r\n");
     while(1) {
-        pc.putc(pc.getc());
+        greentea_serial->putc(greentea_serial->getc());
     }
 }
